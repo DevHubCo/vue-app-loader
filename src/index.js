dc5442ecfcf33878d4ffcadc1a2e83f6e16decf6
Jimmy Fan <fanminjian@gmail.com>
2017-08-17 01:21:11 +1000

    v0.0.1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import _ from 'lodash'
+
+export default {
+  start: (apps) => {
+    $(() => {
+      _.each($('app'), (element) => {
+        let appName = $(element).attr('name')
+        let targetApp = apps[_.upperFirst(_.camelCase(appName))]
+        new Vue(targetApp).$mount(element)
+      })
+    })
+  }
+}
